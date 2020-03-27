d853b5420834bcf4450bb9f8bf6f536a1080dae5
YoussefAmzil <youssefamzil98@gmail.com>
2020-03-27 15:07:56 +0100

    adding svg 2  home page

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
-  <div id="app">
+  <div id="app" class="bg-gray-800 h-screen">
       <navbar />
+      <div class="flex">
+      <home />
+      </div>
   </div>
 </template>
 
 <script>
-import Navbar from '@/components/Navbar.vue'
+import Navbar from '@/components/Navbar.vue';
+import Home from '@/components/Home.vue';
+
 export default {
   name: 'App',
   components: {
-    Navbar
+    Navbar,
+    Home
   }
 }
 </script>
---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="">
+    <div>
+      <img src="img/bike.svg" class="h-48" alt="working">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
