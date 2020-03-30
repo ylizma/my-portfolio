7fcda42a415c7aa83e6d07466eb856c31e178fa2
YoussefAmzil <youssefamzil98@gmail.com>
2020-03-30 22:00:05 +0100

    adding social icons to footer

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="bg-gray-900 rounded text-white overflow-hidden shadow-md mx-4 px-8 h-48 py-4 my-16 sm:my-48 sm:h-64 sm:w-1/3 sm:px-5 sm:mx-20 ">
    
-    <vue-typed-js  class="sm:mt-24 mt-12" :strings="['ⴰⵣⵓⵍ ,^500', 'Hi ,Im Youssef Amzil Im a web Developer  &#9996; &#9996; . ^2000']" :typeSpeed="100" :loop="true">
-      <h2 class="sm:text-xl text-sm"> <span class="text-green-400">$  </span><span class="typing"></span></h2>
+    <vue-typed-js  class="sm:mt-10 mt-10" :strings="['ⴰⵣⵓⵍ ,^500', 'Hi my name is Youssef Amzil, i\'m 22 years old and i live in Agadir, Morocco&#127754; .<br> <em>$  </em> Im a web Developer and Java Enthausiast ☕☕. ^2000']" :typeSpeed="100" :loop="true">
+      <h2 class="sm:text-xl text-sm"> <em class="text-green-400">$  </em><span class="typing"></span></h2>
     </vue-typed-js> 
 
   </div>
@@ -15,5 +15,7 @@ export default {
 </script>
 
 <style >
-/*   */
+em{
+  color: #68d391;
+}
 </style>
