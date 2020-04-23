7810c6fbe47ca9b091692e503b586379539e98ab
YoussefAmzil <youssefamzil98@gmail.com>
2020-04-23 22:03:53 +0000

    change2

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bg-gray-900 rounded text-white overflow-hidden shadow-md mx-4 px-8 h-48 py-4 my-16 lg:my-48 lg:h-64 lg:w-1/3 lg:px-5 lg:mx-20 ">
    
-    <vue-typed-js  class="lg:mt-10 mt-10" :strings="['ⴰⵣⵓⵍ ,^500', 'Hi my name is Youssef Amzil, i\'m 22 years old and i live in Agadir, Morocco&#127754; .<br> <em>$  </em> Im an engineer student, Full stack Developer and Data Science Enthausiast ☕☕. ^2000']" :typeSpeed="80" :loop="true">
+    <vue-typed-js  class="lg:mt-10 mt-10" :strings="['ⴰⵣⵓⵍ ,^500', 'Hi my name is Youssef Amzil, i\'m 22 years old and i live in Agadir, Morocco&#127754; .<br> <em>$  </em> I\'m an engineering student, Full-stack Developer, and Data Science Enthusiast.  ☕☕. ^2000']" :typeSpeed="80" :loop="true">
       <h2 class="lg:text-xl text-sm"> <em class="text-green-400">$  </em><span class="typing"></span></h2>
     </vue-typed-js> 
 
