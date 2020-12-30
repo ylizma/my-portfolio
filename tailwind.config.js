d9a65b57c9e6bed1ef82ed3debf5d572d46c8cb0
ylizma <youssefamzil98@gmail.com>
2020-12-30 15:29:49 +0100

    adding purge css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,23 @@
+require('dotenv').config();
+const enablePurge = process.env.ENABLE_PURGE || false;
 module.exports = {
   theme: {
     extend: {
       spacing: {
-        '70': '20rem',
+        70: "20rem",
       },
       screens: {
-         'lg': {'min': '1200px'},
-         'sm': {'min': '640px', 'max': '767px'},
-         'md': {'min': '768px', 'max': '1023px'},
-
+        lg: { min: "1200px" },
+        sm: { min: "640px", max: "767px" },
+        md: { min: "768px", max: "1023px" },
       },
     },
   },
-  variants: {},
+  variants: {
+    extend: {
+      fontWeight: ["hover", "focus"],
+    },
+  },
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   plugins: [],
-}
+};
